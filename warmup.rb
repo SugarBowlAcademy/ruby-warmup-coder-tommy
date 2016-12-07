181eef1c2c66275cc7492d5c93ffd01f37be926a
coder-tommy <tzpullen@gmail.com>
2016-12-07 15:33:30 -0800

    Create warmup.rb

---
+++ warmup.rb
@@ -0,0 +1,15 @@
+petnames = ["ghost", "enzo", "spice", "spike", "fido"]
+petnames.each do [names]
+  puts names
+end
+
+petnames = ["ghost", "enzo", "spice", "spike", "fido"]
+for [names] in petnames
+  puts names
+end
+
+
+
+=begin
+I prefer the for [names] in petnames version of the loop because it is what I would think to do
+=end
